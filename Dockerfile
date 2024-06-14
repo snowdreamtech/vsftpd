4f3e69ea67d344c9dfac14dad25aca8c094833e6
snowdream <sn0wdr1am@qq.com>
2024-06-14 17:57:35 +0800

    ..........            5

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,17 @@
+FROM snowdreamtech/build-essential:3.20.0 AS builder
+
+ENV LIBPAM_PWDFILE_VERSION 1.0
+
+RUN mkdir /workspace
+WORKDIR /workspace
+RUN apk add --no-cache linux-pam-dev  \ 
+    && wget https://github.com/tiwe-de/libpam-pwdfile/archive/refs/tags/v${LIBPAM_PWDFILE_VERSION}.tar.gz  \ 
+    && tar zxvf v${LIBPAM_PWDFILE_VERSION}.tar.gz  \ 
+    && cd libpam-pwdfile-${LIBPAM_PWDFILE_VERSION}  \ 
+    && make \
+    && cp pam_pwdfile.so ../
+
+
 FROM snowdreamtech/alpine:3.20.0
 
 LABEL maintainer="snowdream <sn0wdr1am@qq.com>"
@@ -10,10 +24,13 @@ ENV FTP_ROOT_PASSWORD= \
     PASV_MIN_PORT= \
     PASV_MAX_PORT=
 
+RUN apk add --no-cache vsftpd openssl
 
-RUN apk add --no-cache vsftpd
+COPY --from=builder /workspace/pam_pwdfile.so /lib/security/
 
 EXPOSE 21 20
+
+COPY pam.d/* /etc/pam.d/
 
 COPY vsftpd/* /etc/vsftpd/
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,54 @@
 set -e
 
 FTP_CONFIG_PATH=/etc/vsftpd/vsftpd.conf
+FTP_PASSWD_PATH=/etc/vsftpd/.passwd
+FTP_VIRTUAL_HOME=/home/virtual
 
 if [ -n "$FTP_MODE" ]; then
     if [ "$FTP_MODE" == "anonymous" ]; then
         FTP_CONFIG_PATH=/etc/vsftpd/vsftpd_anonymous.conf
     elif [ "$FTP_MODE" == "user" ]; then
         FTP_CONFIG_PATH=/etc/vsftpd/vsftpd_user.conf
+
+        # adduser for vsftp
+        if [ -n "$FTP_USER" ] && [ -n "$FTP_PASS" ]; then
+            if id -u $FTP_USER >/dev/null 2>&1; then
+                echo "$FTP_USER:$FTP_PASS" | chpasswd >/dev/null 2>&1
+            else
+                echo -e "$FTP_PASS\n$FTP_PASS" | adduser -h /home/$FTP_USER -s /sbin/nologin $FTP_USER >/dev/null 2>&1
+            fi
+        fi
     elif [ "$FTP_MODE" == "virtual" ]; then
         FTP_CONFIG_PATH=/etc/vsftpd/vsftpd_virtual.conf
+
+        # adduser for virtual
+        if ! id -u "virtual" >/dev/null 2>&1; then
+            adduser -D -h $FTP_VIRTUAL_HOME -s /sbin/nologin virtual >/dev/null 2>&1
+        fi
+
+        # change the password for root
+        if [ -n "$FTP_ROOT_PASSWORD" ]; then
+            sed -i "/^root:/d" $FTP_PASSWD_PATH
+            HASHED_FTP_ROOT_PASSWORD=$(echo -e "$FTP_ROOT_PASSWORD" | openssl passwd -1 -stdin)
+            echo "root:$HASHED_FTP_ROOT_PASSWORD" >>$FTP_PASSWD_PATH
+
+            if [ ! -d $FTP_VIRTUAL_HOME/root ]; then
+                mkdir -p $FTP_VIRTUAL_HOME/root
+                chown -R virtual:virtual $FTP_VIRTUAL_HOME/root
+            fi
+        fi
+
+        # adduser for vsftp
+        if [ -n "$FTP_USER" ] && [ -n "$FTP_PASS" ]; then
+            sed -i "/^$FTP_USER:/d" $FTP_PASSWD_PATH
+            HASHED_FTP_USER_PASSWORD=$(echo -e "$FTP_PASS" | openssl passwd -1 -stdin)
+            echo "$FTP_USER:$HASHED_FTP_USER_PASSWORD" >>$FTP_PASSWD_PATH
+
+            if [ ! -d $FTP_VIRTUAL_HOME/$FTP_USER ]; then
+                mkdir -p $FTP_VIRTUAL_HOME/$FTP_USER
+                chown -R virtual:virtual $FTP_VIRTUAL_HOME/$FTP_USER
+            fi
+        fi
     else
         FTP_CONFIG_PATH=/etc/vsftpd/vsftpd.conf
     fi
@@ -20,15 +60,6 @@ fi
 # change the password for root
 if [ -n "$FTP_ROOT_PASSWORD" ]; then
     echo "root:$FTP_ROOT_PASSWORD" | chpasswd >/dev/null 2>&1
-fi
-
-# adduser for vsftp
-if [ -n "$FTP_USER" ] && [ -n "$FTP_PASS" ]; then
-    if id -u $FTP_USER >/dev/null 2>&1 ; then
-        echo "$FTP_USER:$FTP_PASS" | chpasswd >/dev/null 2>&1
-    else
-        echo -e "$FTP_PASS\n$FTP_PASS" | adduser -h /home/$FTP_USER -s /sbin/nologin $FTP_USER
-    fi
 fi
 
 # passive
