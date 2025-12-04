458de79bab20591d16680682029c0a3ea434d588
snowdream <sn0wdr1am@qq.com>
2025-12-04 19:20:10 +0800

    Add vsftpd configuration and setup scripts for user and virtual FTP modes
    - Created vsftpd configuration files for anonymous, user, and virtual modes.
    - Implemented entrypoint scripts to set up vsftpd based on environment variables.
    - Configured PAM for user authentication using a password file.
    - Added necessary directories and files for virtual user management.
    - Included a banner file for FTP service greeting.
    - Established user and chroot lists for access control.
    - Enabled logging and specified log file paths for FTP transactions.

---
+++ debian/entrypoint.d/11-vsftpd-start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+if [ "$DEBUG" = "true" ]; then echo "→ [vsftpd] Starting vsftpd..."; fi
+
+# start vsftpd
+/usr/sbin/vsftpd $FTP_CONFIG_PATH
+
+if [ "$DEBUG" = "true" ]; then echo "→ [vsftpd] Vsftpd started."; fi
