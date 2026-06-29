7cc73148c23746978b69b65eec98d9cf8837693b
snowdream <snowdream@users.noreply.github.com>
2026-06-29 17:00:59 +0800

    fix(docker): fix shellcheck errors and make entrypoint scripts executable

--- docker/debian/entrypoint.d/10-vsftpd-setup.sh
+++ docker/debian/entrypoint.d/10-vsftpd-setup.sh
@@ -8,9 +8,9 @@ FTP_PASSWD_PATH=/etc/vsftpd/.passwd
 FTP_VIRTUAL_HOME=/home/virtual
 
 if [ -n "$FTP_MODE" ]; then
-  if [ "$FTP_MODE" == "anonymous" ]; then
+  if [ "$FTP_MODE" = "anonymous" ]; then
     FTP_CONFIG_PATH=/etc/vsftpd/vsftpd_anonymous.conf
-  elif [ "$FTP_MODE" == "user" ]; then
+  elif [ "$FTP_MODE" = "user" ]; then
     FTP_CONFIG_PATH=/etc/vsftpd/vsftpd_user.conf
 
     # adduser for vsftp
@@ -18,10 +18,10 @@ if [ -n "$FTP_MODE" ]; then
       if id -u "$FTP_USER" >/dev/null 2>&1; then
         echo "$FTP_USER:$FTP_PASS" | chpasswd >/dev/null 2>&1
       else
-        echo -e "$FTP_PASS\n$FTP_PASS" | adduser -h /home/"$FTP_USER" -s /sbin/nologin "$FTP_USER" >/dev/null 2>&1
+        printf '%s\n%s\n' "$FTP_PASS" "$FTP_PASS" | adduser -h /home/"$FTP_USER" -s /sbin/nologin "$FTP_USER" >/dev/null 2>&1
       fi
     fi
-  elif [ "$FTP_MODE" == "virtual" ]; then
+  elif [ "$FTP_MODE" = "virtual" ]; then
     FTP_CONFIG_PATH=/etc/vsftpd/vsftpd_virtual.conf
 
     # adduser for virtual
@@ -32,7 +32,7 @@ if [ -n "$FTP_MODE" ]; then
     # change the password for root
     if [ -n "$FTP_ROOT_PWD" ]; then
       sed -i "/^root:/d" $FTP_PASSWD_PATH
-      HASHED_FTP_ROOT_PWD=$(echo -e "$FTP_ROOT_PWD" | openssl passwd -1 -stdin)
+      HASHED_FTP_ROOT_PWD=$(printf '%s\n' "$FTP_ROOT_PWD" | openssl passwd -1 -stdin)
       echo "root:$HASHED_FTP_ROOT_PWD" >>$FTP_PASSWD_PATH
 
       if [ ! -d $FTP_VIRTUAL_HOME/root ]; then
@@ -44,7 +44,7 @@ if [ -n "$FTP_MODE" ]; then
     # adduser for vsftp
     if [ -n "$FTP_USER" ] && [ -n "$FTP_PASS" ]; then
       sed -i "/^$FTP_USER:/d" $FTP_PASSWD_PATH
-      HASHED_FTP_USER_PASSWORD=$(echo -e "$FTP_PASS" | openssl passwd -1 -stdin)
+      HASHED_FTP_USER_PASSWORD=$(printf '%s\n' "$FTP_PASS" | openssl passwd -1 -stdin)
       echo "$FTP_USER:$HASHED_FTP_USER_PASSWORD" >>$FTP_PASSWD_PATH
 
       if [ ! -d $FTP_VIRTUAL_HOME/"$FTP_USER" ]; then
@@ -68,19 +68,19 @@ fi
 # pasv_address
 if [ -n "$PASV_ADDRESS" ]; then
   sed -i "/pasv_address/d" $FTP_CONFIG_PATH
-  echo -e "\npasv_address=$PASV_ADDRESS" >>$FTP_CONFIG_PATH
+  printf "\npasv_address=%s\n" "$PASV_ADDRESS" >>$FTP_CONFIG_PATH
 fi
 
 # pasv_min_port
 if [ -n "$PASV_MIN_PORT" ]; then
   sed -i "/pasv_min_port/d" $FTP_CONFIG_PATH
-  echo -e "\npasv_min_port=$PASV_MIN_PORT" >>$FTP_CONFIG_PATH
+  printf "\npasv_min_port=%s\n" "$PASV_MIN_PORT" >>$FTP_CONFIG_PATH
 fi
 
 # pasv_max_port
 if [ -n "$PASV_MAX_PORT" ]; then
   sed -i "/pasv_max_port/d" $FTP_CONFIG_PATH
-  echo -e "\npasv_max_port=$PASV_MAX_PORT" >>$FTP_CONFIG_PATH
+  printf "\npasv_max_port=%s\n" "$PASV_MAX_PORT" >>$FTP_CONFIG_PATH
 fi
 
 if [ "$DEBUG" = "true" ]; then echo "→ [vsftpd] Vsftpd has been set up."; fi
